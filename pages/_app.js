0d5984c5041fa8f92fff0a51185b3a5c4888cc92
k0825 <kazukkiyo@icloud.com>
2022-09-25 22:46:33 +0900

    カートに追加まで実装

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,56 @@ import Cookie from "js-cookie";
 
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState(null);
+  const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const addItem = (item) => {
+    const newItem = items.find((i) => i.id === item.id);
+    if (!newItem) {
+      item = { ...item, attributes: { ...item.attributes, quantity: 1 } };
+      console.log(item);
+      setItems([...items, item]);
+    } else {
+      const newItems = items.map((item) => {
+        if (item.id === newItem.id) {
+          item = {
+            ...item,
+            attributes: {
+              ...item.attributes,
+              quantity: item.attributes.quantity + 1,
+            },
+          };
+        }
+        return item;
+      });
+      setItems(newItems);
+    }
+    setTotal(total + item.attributes.price);
+    Cookie.set("cartItems", JSON.stringify(items));
+  };
+
+  const removeItem = (item) => {
+    const newItem = items.find((i) => i.id === item.id);
+
+    const newItems = [...items];
+    const index = newItems.findIndex((i) => i.id === newItem.id);
+    newItems.splice(index, 1);
+
+    if (newItem.attributes.quantity > 1) {
+      item = {
+        ...item,
+        attributes: {
+          ...item.attributes,
+          quantity: item.attributes.quantity - 1,
+        },
+      };
+      setItems([...newItems, item]);
+    } else {
+      setItems(newItems);
+    }
+    setTotal(total - item.attributes.price);
+    Cookie.set("cartItems", JSON.stringify(items));
+  };
 
   useEffect(() => {
     const token = Cookie.get("token");
@@ -15,7 +65,6 @@ function MyApp({ Component, pageProps }) {
           Authorization: `Bearer ${token}`,
         },
       }).then(async (res) => {
-        console.log(res);
         if (!res.ok) {
           Cookie.remove("token");
           setUser(null);
@@ -26,8 +75,36 @@ function MyApp({ Component, pageProps }) {
       });
     }
   }, []);
+
+  useEffect(() => {
+    const cartItems = Cookie.get("cartItems");
+
+    // console.log(cartItems);
+    let total = 0;
+
+    if (cartItems !== "undefined") {
+      JSON.parse(cartItems).forEach((item) => {
+        total += item.attributes.price * item.attributes.quantity;
+      });
+
+      setItems(JSON.parse(cartItems));
+      setTotal(total);
+    }
+  }, []);
+
+  // console.log(items);
+  // console.log(items.map((item) => item.attributes.quantity));
+
   return (
-    <AppContext.Provider value={{ user: user, setUser: setUser }}>
+    <AppContext.Provider
+      value={{
+        user: user,
+        setUser: setUser,
+        addItem: addItem,
+        removeItem: removeItem,
+        cart: { items: items, total: total },
+      }}
+    >
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/restaurants.js
+++ pages/restaurants.js
@@ -1,6 +1,9 @@
 import { Button, CardBody, CardImg, CardTitle, Col, Row } from "reactstrap";
 import { gql, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
+import Cart from "../components/Cart";
+import { useContext } from "react";
+import AppContext from "../context/AppContext";
 
 const GET_RESTAURANT_DISHES = gql`
   query GetRestaurantDishes($id: ID!) {
@@ -16,6 +19,8 @@ const GET_RESTAURANT_DISHES = gql`
               attributes {
                 name
                 description
+                quantity
+                price
                 image {
                   data {
                     attributes {
@@ -33,6 +38,7 @@ const GET_RESTAURANT_DISHES = gql`
 `;
 
 const Restaurants = () => {
+  const appContext = useContext(AppContext);
   const router = useRouter();
   const { loading, error, data } = useQuery(GET_RESTAURANT_DISHES, {
     variables: { id: router.query.id },
@@ -45,24 +51,53 @@ const Restaurants = () => {
       <>
         <h1>{data.restaurant.data.attributes.name}</h1>
         <Row>
-          {data.restaurant.data.attributes.dishes.data.map((dish) => (
-            <Col xs="6" sm="4" key={dish.id} style={{ padding: 0 }}>
-              <CardBody style={{ margin: "0 10px" }}>
-                <CardImg
-                  src={`${process.env.NEXT_PUBLIC_API_URL}${dish.attributes.image.data.attributes.formats.large.url}`}
-                  top={true}
-                  style={{ height: 250 }}
-                />
-                <CardTitle>{dish.attributes.name}</CardTitle>
-                <CardTitle>{dish.attributes.description}</CardTitle>
-              </CardBody>
-              <div className="card-footer">
-                <Button outline color="primary">
-                  + カートに入れる
-                </Button>
-              </div>
-            </Col>
-          ))}
+          {data.restaurant.data.attributes.dishes.data.map((dish) => {
+            // console.log(dish.attributes.quantity);
+            return (
+              <Col xs="6" sm="4" key={dish.id} style={{ padding: 0 }}>
+                <CardBody style={{ margin: "0 10px" }}>
+                  <CardImg
+                    src={`${process.env.NEXT_PUBLIC_API_URL}${dish.attributes.image.data.attributes.formats.large.url}`}
+                    top={true}
+                    style={{ height: 250 }}
+                  />
+                  <CardTitle>{dish.attributes.name}</CardTitle>
+                  <CardTitle>{dish.attributes.description}</CardTitle>
+                </CardBody>
+                <div className="card-footer">
+                  <Button
+                    outline
+                    color="primary"
+                    onClick={() => appContext.addItem(dish)}
+                  >
+                    + カートに入れる
+                  </Button>
+                </div>
+              </Col>
+            );
+          })}
+          <style jsx>
+            {`
+              a {
+                color: hite;
+              }
+              a:link {
+                text-decoration: none;
+                color: white;
+              }
+              a:hover {
+                color: white;
+              }
+              .card-columns {
+                column-connt: 3;
+              }
+            `}
+          </style>
+          <Col>
+            <div>
+              <Cart />
+            </div>
+          </Col>
         </Row>
       </>
     );
